1595cbf96977bbf1f8c4eabad41b2d688548a19d
Suryan S <suryannasa@gmail.com>
2023-06-10 03:09:37 +0530

    ADDED modified allowed host for deployment

--- TaskMaster/settings.py
+++ TaskMaster/settings.py
@@ -29,7 +29,10 @@ DEBUG = 'RENDER' not in os.environ
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
